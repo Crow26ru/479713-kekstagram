e54415e8705a61b62b3de40cc6f449a45a66e2ff
Crow26 <7tacson7@gmail.com>
2018-12-03 20:27:46 +0300

    Реализован drag-n-grop пина слайдера

---
+++ js/drag-slider.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function () {
+  var lineEffectSliderElement = document.querySelector('.effect-level__line');
+  var pinEffectSliderElement = lineEffectSliderElement.querySelector('.effect-level__pin');
+
+  var mouseDownHandler = function (evt) {
+    evt.preventDefault();
+    var startPositionX = evt.clientX;
+
+    var mouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shiftX = startPositionX - pinEffectSliderElement.offsetLeft;
+      var stylesLineEffectSlider = getComputedStyle(lineEffectSliderElement);
+      var maxWidth = parseInt(stylesLineEffectSlider.width, 10);
+
+      var getPositionInnerMinMaxValues = function (value, min, max) {
+        return Math.min(Math.max(value, min), max);
+      };
+
+      startPositionX = moveEvt.clientX;
+
+      var newX = getPositionInnerMinMaxValues(startPositionX - shiftX, 0, maxWidth);
+
+      pinEffectSliderElement.style.left = newX + 'px';
+    };
+    
+    var mouseUpHandler = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);    
+  };
+
+  pinEffectSliderElement.addEventListener('mousedown', mouseDownHandler);
+})();
