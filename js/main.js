1d5c45e5d86ffb30023d64c90693567f5f205311
Crow26 <7tacson7@gmail.com>
2018-12-05 15:55:03 +0300

    Удален закомментированный код, который был перенесен в модуль pictures.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,5 @@
 'use strict';
-/*
-var COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-var DESCRIPTIONS = [
-  'Тестим новую камеру!',
-  'Затусили с друзьями на море',
-  'Как же круто тут кормят',
-  'Отдыхаем...',
-  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-  'Вот это тачка!'
-];
-var TOTAL_PHOTOS_FROM_RANDOM_USERS = 25;
-var MIN_LIKES = 15;
-var MAX_LIKES = 200;
-var MIN_COMMENTS = 5;
-var MAX_COMMENTS = 15;
-*/
+
 var MIN_URL_ICON_IMAGE = 1;
 var MAX_URL_ICON_IMAGE = 6;
 var AVATAR_RANDOM_USER_ALT = 'Аватар комментатора фотографии';
@@ -29,15 +7,7 @@ var AVATAR_RANDOM_USER_WIDTH = 35;
 var AVATAR_RANDOM_USER_HEIGHT = 35;
 
 var ESC_KEYCODE = 27;
-// var photosGuests = [];
-// Шаблон откуда берем разметку
-/*
-var pictureTemplateElement = document.querySelector('#picture')
-     .content
-     .querySelector('.picture');
-// Куда будем вставлять разметку из шаблона
-var picturesListElement = document.querySelector('.pictures');
-*/
+
 // Секция загрузки нового изображения
 var fileUploadElement = document.querySelector('#upload-file');
 var fileUploadCancelElement = document.querySelector('#upload-cancel');
@@ -50,35 +20,6 @@ var uploadImageElement = document.querySelector('.img-upload__preview img');
 // Здесь надо будет исправить экспорт!
 window.uploadImageElement = uploadImageElement;
 
-// ФУНКЦИИ ДЛЯ СОЗДАНИЯ ИНФОРМАЦИИ О ФОТОГАФИЯХ ОТ СЛУЧАЙНЫХ ПОЛЬЗОВАТЕЛЕЙ
-/*
-var createDataInArray = function (arr) {
-  for (var i = 1; i <= TOTAL_PHOTOS_FROM_RANDOM_USERS; i++) {
-    var photoInfo = {
-      url: getUrlForPhoto(i),
-      likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-      comments: getComments(MIN_COMMENTS, MAX_COMMENTS),
-      description: getRandomElementArray(DESCRIPTIONS)
-    };
-    arr.push(photoInfo);
-  }
-};
-
-var getUrlForPhoto = function (count) {
-  return 'photos/' + count + '.jpg';
-};
-
-var getComments = function (min, max) {
-  var totalComments = getRandomNumber(min, max);
-  var comments = [];
-
-  for (var i = 0; i < totalComments; i++) {
-    comments.push(getRandomElementArray(COMMENTS));
-  }
-
-  return comments;
-};
-*/
 // ФУНКЦИИ ДЛЯ РАБОТЫ С ЭЛЕМЕНТАМИ МАССИВА
 
 var searchElementArray = function (arr, url) {
@@ -90,17 +31,6 @@ var searchElementArray = function (arr, url) {
   return null;
 };
 
-// ФУНКЦИИ ВОЗВРАЩАЮЩИЕ СЛУЧАЙНЫЙ РЕЗУЛЬТАТ
-/*
-var getRandomNumber = function (min, max) {
-  var range = max - min + 1;
-  return Math.floor(Math.random() * range) + min;
-};
-
-var getRandomElementArray = function (arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
-*/
 // ФУНКЦИИ ДЛЯ ВЗАИМОДЕЙСТВИЯ С DOM
 
 var createElement = function (tag, className, text) {
@@ -153,24 +83,7 @@ var insertCommentListElement = function (elem) {
   var commentsElement = document.querySelector('.social__comments');
   commentsElement.appendChild(createCommentListElement(elem));
 };
-/*
-var insertPhotosRandomUsersElements = function () {
-  var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < TOTAL_PHOTOS_FROM_RANDOM_USERS; i++) {
-    var url = photosGuests[i].url;
-    var likes = photosGuests[i].likes;
-    var comments = photosGuests[i].comments;
-    var imageRandomElement = pictureTemplateElement.cloneNode(true);
-    imageRandomElement.querySelector('.picture__img').src = url;
-    imageRandomElement.querySelector('.picture__likes').textContent = likes;
-    imageRandomElement.querySelector('.picture__comments').textContent = comments.length;
-    fragment.appendChild(imageRandomElement);
-  }
-
-  picturesListElement.appendChild(fragment);
-};
-*/
 var showBigPictureElement = function (elem) {
   var alt = 'Фото рандомного пользователя';
   var bigPictureElement = document.querySelector('.big-picture');
@@ -361,12 +274,6 @@ var hashtagInputHandler = function (evt) {
     return true;
   }
 };
-
-// ВЫЗОВ ФУНКЦИЙ
-
-// createDataInArray(photosGuests);
-// insertPhotosRandomUsersElements();
-// showBigPictureElement(searchElementArray(photosGuests, 'photos/1.jpg'));
 
 // ОБРАБОТЧИКИ СОБЫТИЙ
 
