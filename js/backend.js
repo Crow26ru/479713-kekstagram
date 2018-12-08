3e02e00e6e834e187376b72ad1ec6a6550620b50
Crow26 <7tacson7@gmail.com>
2018-12-08 09:01:59 +0300

    Убраны лишние пробелы

--- js/backend.js
+++ js/backend.js
@@ -3,12 +3,12 @@
 (function () {
   var URL_DOWNLOAD = 'https://js.dump.academy/kekstagram/data';
   var URL_UPLOAD = 'https://js.dump.academy/kekstagram';
-  
+
   var download = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.timeout = '10000';
-    
+
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onLoad(xhr.response);
@@ -16,24 +16,24 @@
         onError('Ошибка загрузки данных.');
       }
     });
-    
+
     xhr.addEventListener('error', function () {
       onError('Ошибка ' + xhr.status + ': ' + xhr.statusText);
     });
-    
+
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс')ж
     });
-    
+
     xhr.open('GET', URL_DOWNLOAD);
     xhr.send();
   };
-  
+
   var upload = function (data, onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.timeout = '10000';
-    
+
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onLoad(xhr.response);
@@ -49,7 +49,7 @@
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс')ж
     });
-    
+
     xhr.open('POST', URL_UPLOAD);
     xhr.send();
   };
